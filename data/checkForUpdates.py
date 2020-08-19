532f0e20fc51a9236dde5d9e47c034be90adf5f6
AndreasKarageorgos <44126202+AndreasKarageorgos@users.noreply.github.com>
2020-08-19 16:59:56 +0300

    Add files via upload

---
+++ data/checkForUpdates.py
@@ -0,0 +1,28 @@
+import requests
+
+def check():
+    prox = {
+        "https": "socks5://127.0.0.1:9050"
+    }  
+
+    program = "https://github.com/AndreasKarageorgos/Anonymous-Chat"
+
+    server = "Alpha 1.0.1"
+    client = "Alpha 1.0.1"
+    
+    try:
+        r = requests.get("https://raw.githubusercontent.com/AndreasKarageorgos/Anonymous-Chat/master/VERSIONS", proxies = prox).text.split()
+    except:
+        return "Connection to Tor network could not be established."
+    
+    sv = r[1]+" "+r[2]
+    cv = r[1]+" "+r[5]
+
+    if (sv!=server and cv!=client):
+        return f"Update found for client and server !!!\n\nYou can download it here => {program}"
+    elif(sv!=server):
+        return f"Update found for server !!!\n\nYou can download it here {program}"
+    elif(cv!=client):
+        return f"Update found for client !!!\n\nYou can download it here {program}"
+    
+    return"No updates found !!!"
