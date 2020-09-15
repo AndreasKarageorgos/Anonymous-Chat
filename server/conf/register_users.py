ae90145e9082a7aa494ca5528a2ee69971136299
Andreas Karageorgos <44126202+AndreasKarageorgos@users.noreply.github.com>
2020-09-15 07:03:37 +0300

    Add files via upload

--- server/conf/register_users.py
+++ server/conf/register_users.py
@@ -1,11 +1,13 @@
 from hashlib import sha512
 
 def reg_user(uname,passwd,users):
+    if b" " in uname or b"\n" in uname:
+        return False
     with open("conf/users","a") as f:
         try:
             comp = sha512(uname+passwd).hexdigest()
             uname = uname.decode("ascii")
-            if uname not in users:
+            if uname.lower() not in [x.lower() for x in users]:
                 f.write(f"{uname}:{comp}\n")
                 users.update({uname:comp})
                 return True
